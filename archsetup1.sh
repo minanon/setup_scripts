f94ccf24af9596bb3722b3ad782ec34cac34ab54
Sumi Minano <sumi.minano@gmail.com>
2016-07-28 14:00:21 +0900

    arch

--- archsetup1.sh
+++ archsetup1.sh
@@ -23,3 +23,7 @@ arch-chroot /mnt
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub --boot-directory=/boot/efi/EFI --recheck --debug
 sed -i -e 's/GRUB_TIMEOUT.*/GRUB_TIMEOUT=0/' /etc/default/grub
 grub-mkconfig -o /boot/efi/EFI/grub/grub.cfg
+
+umount /dev/sda1
+umount /dev/sda2
+umount /dev/sda4
