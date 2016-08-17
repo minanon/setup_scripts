6336a197bf9866bb38a52bf6e61b78f706b8bf09
Sumi Minano <sumi.minano@gmail.com>
2016-08-17 15:12:20 +0900

    fix arch setup

--- archsetup1.sh
+++ archsetup1.sh
@@ -24,6 +24,8 @@ grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub 
 sed -i -e 's/GRUB_TIMEOUT.*/GRUB_TIMEOUT=0/' /etc/default/grub
 grub-mkconfig -o /boot/efi/EFI/grub/grub.cfg
 
+exit
+
 umount /dev/sda1
 umount /dev/sda2
 umount /dev/sda4
